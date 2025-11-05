f1f3f26bf38f56a84842f5ddd0eb8c3d86178d47
joel <joelsantossouza08@gmail.com>
2025-11-05 19:13:44 +0000

    Spliting the function of rendering the fdf grid into horizontal and vertical draw. I chose this way to improve efficiency of the program

--- srcs/renderers/render_fdf.c
+++ srcs/renderers/render_fdf.c
@@ -6,34 +6,62 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 15:11:03 by joesanto          #+#    #+#             */
-/*   Updated: 2025/11/05 18:04:43 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/11/05 19:11:56 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	render_fdf(t_image image, t_map map, t_fdf fdf, t_linedrawer *drawline)
+static inline
+void	render_fdf_horizontal_lines(t_image image, t_map map, t_fdf fdf, t_linedrawer *drawline)
 {
+	t_point			p;
+	const int		start_x = fdf.position.x + fdf.spacing;
 	const t_point	end = (t_point){
 		fdf.position.x + map.width * fdf.spacing,
 		fdf.position.y + map.height * fdf.spacing,
 	};
-	t_point			p;
 
-	if (map.width == 1 && map.height == 1)
-		putpixel(image, fdf.position.x, fdf.position.y, WHITE);
 	p.y = fdf.position.y;
 	while (p.y < end.y)
 	{
-		p.x = fdf.position.x;
+		p.x = start_x;
 		while (p.x < end.x)
 		{
-			if (p.y + fdf.spacing < end.y)
-				drawline(image, p, (t_point){p.x, p.y + fdf.spacing}, WHITE);
-			if (p.x + fdf.spacing < end.x)
-				drawline(image, p, (t_point){p.x + fdf.spacing, p.y}, WHITE);
+			drawline(image, (t_point){p.x - fdf.spacing, p.y}, p, WHITE);
 			p.x += fdf.spacing;
 		}
 		p.y += fdf.spacing;
 	}
+}
+
+static inline
+void	render_fdf_vertical_lines(t_image image, t_map map, t_fdf fdf, t_linedrawer *drawline)
+{
+	t_point			p;
+	const int		start_y = fdf.position.y + fdf.spacing;
+	const t_point	end = (t_point){
+		fdf.position.x + map.width * fdf.spacing,
+		fdf.position.y + map.height * fdf.spacing,
+	};
+
+	p.x = fdf.position.x;
+	while (p.x < end.x)
+	{
+		p.y = start_y;
+		while (p.y < end.y)
+		{
+			drawline(image, (t_point){p.x, p.y - fdf.spacing}, p, WHITE);
+			p.y += fdf.spacing;
+		}
+		p.x += fdf.spacing;
+	}
+}
+
+void	render_fdf(t_image image, t_map map, t_fdf fdf, t_linedrawer *drawline)
+{
+	if (map.width == 1 && map.height == 1)
+		return (putpixel(image, fdf.position.x, fdf.position.y, WHITE));
+	render_fdf_horizontal_lines(image, map, fdf, drawline);
+	render_fdf_vertical_lines(image, map, fdf, drawline);
 }
