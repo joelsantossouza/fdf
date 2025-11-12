5d2a790723191feef5e3fe657c65d174e29d8bc0
Joel Santos Souza <joesanto@c2r2s1.42porto.com>
2025-11-12 12:35:28 +0000

    Doing the hook keys

--- includes/voxelspace.h
+++ includes/voxelspace.h
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 14:58:28 by joesanto          #+#    #+#             */
-/*   Updated: 2025/11/11 17:38:01 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/11/12 12:34:59 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,14 @@
 
 # include "fdf.h"
 
+# define QUADRANT	1.5707963267948966
 # define GRAVITY	2
+
+# define KEY_W			1
+# define KEY_S			2
+# define KEY_D			4
+# define KEY_A			8
+# define ARROW_RIGHT	8
 
 typedef struct s_dpoint
 {
@@ -52,11 +59,23 @@ typedef struct s_camera
 typedef struct s_player
 {
 	t_dpoint	*position;
+	t_camera	*cam;
+	t_trig		axis_y;
+	t_trig		axis_x;
+	double		angle;
 	double		height;
 	double		speed;
 	double		zforce;
 	double		climb_max;
+	double		sensibility;
 }	t_player;
+
+typedef struct s_vox
+{
+	t_player	*player;
+	t_map		*map;
+	int			keyboard;
+}	t_vox;
 
 // PARSERS
 int		parse_voxel_file(const char *color_file, const char *altitude_file, t_map *map, int heightscale);
@@ -65,7 +84,7 @@ int		parse_voxel_file(const char *color_file, const char *altitude_file, t_map *
 void	gravity(double *height, double *zforce, int floor);
 
 // MOTIONS
-void move_player(t_player *player, t_trig *angle, t_map *map);
+void	move_player(t_player *player, t_trig *angle, t_map *map);
 
 // RENDER
 void	render_voxelspace(t_image *img, t_map *map, t_camera *cam);
---
+++ srcs/motions/player_motions.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_motions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 13:56:54 by joesanto          #+#    #+#             */
+/*   Updated: 2025/11/12 12:33:15 by joesanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "voxelspace.h"
+#include <math.h>
+
+void	move_player(t_player *player, double cosine, double sine, t_map *map)
+{
+	const int	width = map->width;
+	const int	height = map->height;
+	t_dpoint	*position;
+	t_dpoint	next;
+	double		height_difference;
+
+	position = player->position;
+	next.x = position->x + cosine * player->speed;
+	next.y = position->y + sine * player->speed;
+	if (next.x < 0 || next.x >= width || next.y < 0 || next.y >= height)
+		return ;
+	next.z = map->altitude[width * (int)next.y + (int)next.x] + player->height;
+	height_difference = next.z - position->z;
+	if (height_difference <= player->climb_max)
+	{
+		if (height_difference > 0)
+			position->z = next.z;
+		position->x = next.x;
+		position->y = next.y;
+	}
+}
+
+void	rotate_player(t_player *player, double rotation)
+{
+	const int	zfar = player->cam->zfar;
+	double		angle;
+	t_trig		axis_y;
+	t_fov		*fov;
+
+
+	player->angle += rotation * player->sensibility;
+	angle = player->angle;
+	player->axis_y = (t_trig){cos(angle), sin(angle)};
+	angle += QUADRANT;
+	player->axis_x = (t_trig){cos(angle), sin(angle)};
+	axis_y = player->axis_y;
+	fov = &player->cam->fov;
+	fov->plx = axis_y.cos * zfar + axis_y.sin * zfar;
+	fov->ply = axis_y.sin * zfar - axis_y.cos * zfar;
+	fov->plx = axis_y.cos * zfar - axis_y.sin * zfar;
+	fov->ply = axis_y.sin * zfar + axis_y.cos * zfar;
+}
+
+int	player_motions(t_vox *vox)
+{
+	const t_player	*player = vox->player;
+	const t_trig	axis_y = player->axis_y;
+	const t_trig	axis_x = player->axis_x;
+	const int		keyboard = vox->keyboard;
+
+	if (keyboard & KEY_W)
+		move_player(&player, axis_y.cos, axis_y.sin, &map);
+	if (keyboard & KEY_S)
+		move_player(&player, -axis_y.cos, -axis_y.sin, &map);
+	if (keyboard & KEY_D)
+		move_player(&player, axis_x.cos, axis_x.sin, &map);
+	if (keyboard & KEY_A)
+		move_player(&player, -axis_x.cos, -axis_x.sin, &map);
+	if (keyboard & ARROW_RIGHT)
+		rotate_player(&player, 1);
+	if (keyboard & ARROW_LEFT)
+		rotate_player(&player, -1);
+}
