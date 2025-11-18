66165f70ee9bf2b1528dad3668178253d1b6b18f
joel <joelsantossouza08@gmail.com>
2025-11-18 23:30:51 +0000

    Adding the standard isometric projection at start of rendering

--- srcs/inits/init_fdf.c
+++ srcs/inits/init_fdf.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 20:00:01 by joesanto          #+#    #+#             */
-/*   Updated: 2025/11/18 23:12:49 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/11/18 23:29:39 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@ int	init_fdf(t_fdf *fdf, t_pos pos, t_map *map, t_img *img)
 	fdf->center.y = -((map->height >> 1) * 15);
 	fdf->pos = pos;
 	// FIX ISOMETRIC PROJECTION AND ZOOM
-	axis->angle_x = 0;
-	axis->x = (t_trig){0, 1};
-	axis->angle_y = 0.7853981634;
-	axis->y = (t_trig){0.70710678, 0.70710678};
-	axis->angle_z = 0.6154729074;
-	axis->z = (t_trig){0.57734472, 0.81650051};
+	axis->angle_x = 0.6154729074;
+	axis->x = (t_trig){0.57734472, 0.81650051};
+	axis->angle_y = 0;
+	axis->y = (t_trig){0, 1};
+	axis->angle_z = 0.7853981634;
+	axis->z = (t_trig){0.70710678, 0.70710678};
 	fdf->drawline = bresenham_drawline;
 	fdf->zoom = 1;
 	fdf->max_zoom = hypotenuse.y / (hypotenuse.x * 0.5);
